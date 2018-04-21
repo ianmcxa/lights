4a32afc28c80028c039954f3ca1fd41c3da9203c
Ian Mcxa <ianmcxa@tutanota.com>
2018-04-21 12:23:07 -0400

    Rewrote lctl in go

---
+++ lctl/lctl.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type State struct {
+	Light int `json:light`
+	State int `json:state`
+}
+
+func main() {
+	if len(os.Args) != 3 {
+		exit()
+	}
+
+	light, errLight := strconv.Atoi(os.Args[1])
+	state, errState := strconv.Atoi(os.Args[2])
+
+	if errLight != nil || errState != nil {
+		exit()
+	}
+
+	sendCode(State{light, state})
+}
+
+func sendCode(state State) {
+	body, _ := json.Marshal(state)
+	http.Post("http://[2606:a000:1127:4082:ba27:ebff:fe59:23c]/update",
+		"application/json", bytes.NewBuffer(body))
+}
+
+func exit() {
+	fmt.Println("usage: lctl <light> <state>")
+	os.Exit(1)
+}
